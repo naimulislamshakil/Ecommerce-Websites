70a49ecec743c3bab19aa11abfff98d0002734d5
Naimul Islam <ms0413354@gmail.com>
2024-08-06 22:02:04 +0600

    50% complete footer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Header from './Components/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Footer from './Components/Footer/Footer';
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 			</Routes>
+			<Footer />
 		</div>
 	);
 }
---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import PhoneInTalkOutlinedIcon from '@mui/icons-material/PhoneInTalkOutlined';
+import { Button } from '@mui/material';
+
+const Footer = () => {
+	return (
+		<footer className="container mb-4">
+			<div className="row">
+				<div className="col-md-3 d-flex justify-content-start align-items-center phone">
+					<Button>
+						<PhoneInTalkOutlinedIcon />
+					</Button>
+
+					<div>
+						<h4>8 800 555-55</h4>
+						<small>Working 8:00 - 22:00</small>
+					</div>
+				</div>
+				<div className="col-md-9 d-flex justify-content-end align-items-center">
+					<div
+						style={{
+							lineHeight: '5px',
+							marginRight: '15px',
+						}}
+					>
+						<p>Download App on Mobile :</p>
+						<small style={{ color: 'rgb(137, 137, 137)' }}>
+							15% discount on your first purchase
+						</small>
+					</div>
+
+					<img
+						className="me-2"
+						src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/google-play.png"
+						alt=""
+					/>
+					<img
+						src="https://klbtheme.com/bacola/wp-content/uploads/2021/04/app-store.png"
+						alt=""
+					/>
+				</div>
+			</div>
+		</footer>
+	);
+};
+
+export default Footer;
